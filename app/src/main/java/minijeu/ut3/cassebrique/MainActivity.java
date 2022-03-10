33f0a8f962eecc1408123c05db060501705a78b6
Fixiss <leo.segond@orange.fr>
2022-03-10 16:25:14 +0100

    first commit

---
+++ app/src/main/java/minijeu/ut3/cassebrique/MainActivity.java
@@ -0,0 +1,21 @@
+package minijeu.ut3.cassebrique;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
